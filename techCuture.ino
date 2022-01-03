1c46627855b6863ce3c0f9488ca1d5e47fa1f17b
TalkoAvigad <60072997+TalkoAvigad@users.noreply.github.com>
2022-01-03 20:52:59 +0200

    Tag 1
    The first code is run well, now we need to chack with the hardwear

--- techCuture.ino
+++ techCuture.ino
@@ -2,7 +2,7 @@
 
 Servo servo_one;  // First Servo Objsect;
 Servo servo_two;  // Second Servo Objsect;
-bool CheckInput = true;
+
 int head_unit_one_pos = 0;    // variable to store the servo position
 int head_unit_one_pin = 8;    // pin number 8 for the first head unit
 int head_unit_two_pos = 0;    // variable to store the servo position
@@ -14,30 +14,44 @@ const int stepPin = 3;
 const int stepsToMove = 1000;
 int delayMicro=600;            //250-600 Normal speed per quarter step
 
+int headUnitOnePosDown = 0;
+int headUnitOnePosUp = 180;
+
+int headUnitTwoPosDown = 0;
+int headUnitTwoPosUp = 180;
+
 
 void setup() {
   Serial.begin(9600);
-  servo_one.attach(head_unit_one_pin);  // attaches the servo of the head_unit_one
-  servo_two.attach(head_unit_two_pin);  // attaches the servo of the head_unit_two
+  servo_one.attach(head_unit_one_pin);  // attaches the servo of the head_unit_one pin 8
+  servo_two.attach(head_unit_two_pin);  // attaches the servo of the head_unit_two pin 10
   pinMode(stepPin, OUTPUT);
   pinMode(dirPin, OUTPUT);
 }
 
 void loop(){
   NumberOfLevel = 0;
-  CheckInput = true;
   
   if (Serial.available() > 0) {
-//    if (NumberOfLevel == 0){
-//    Menu(NumberOfLevel);
-//    }
-//    Serial.println("Please select the step:");
     NumberOfLevel = Serial.read();
-//    CheckInputStep(NumberOfLevel);
-//    Serial.println(NumberOfLevel);
   }
   
   switch (NumberOfLevel){
+    case '0':
+      Serial.println("Run for all step");
+      LoweringNeedleDown();
+      delay(1000); // Wait a second
+      ServoMotorWrapped();
+      delay(1000); // Wait a second
+      NeedleRotation();
+      delay(1000); // Wait a second
+      LiftingNeedleUp();
+      delay(1000); // Wait a second
+      NeedleRotationBack();
+      delay(1000); // Wait a second
+      ServoMotorWrappedBack();
+      delay(1000); // Wait a second
+      break;
     case '1':
       Serial.println("Step 1: Lower the needle");
       LoweringNeedleDown();
@@ -64,42 +78,22 @@ void loop(){
       
     case '5':
       Serial.println("Step 5: Rotate the needle clockwise 180 degrees back");
-      NeedleRotation();
+      NeedleRotationBack();
       Serial.println("Step 5 done");
       break;
       
     case '6':
-      Serial.println("Step 6:  ");
-
+      Serial.println("Step 6: Servo motor wrapped in thread on the needle back");
+      ServoMotorWrappedBack();
       Serial.println("Step 6 done");
       break;
   }
 }
 
-int Menu(int i_NumberOfLevel){
-  Serial.println("Welcome to TechCouture.");
-  delay(1500);
-  Serial.flush();
-  Serial.println("Step 1: Lower the needle");
-  Serial.println("Step 2: Servo motor wrapped in thread on the needle");
-  Serial.println("Step 3: Rotate the needle counterclockwise 180 degrees");
-  Serial.println("Step 4: Lifting needle up back");
-  Serial.println("Step 5: Rotate the needle clockwise 180 degrees back");
-  Serial.println("Step 6: ");
-}
-
-bool CheckInputStep(int i_GetStepNumber){
-  if  (i_GetStepNumber >= 1 && i_GetStepNumber <= 6){
-    bool CheckInput = false;
-  }
-  else{
-    Serial.println("Worng Input!");
-  }
-}
 
 void LoweringNeedleDown(){
-  for (head_unit_one_pos = 0; head_unit_one_pos <= 180; head_unit_one_pos += 5){    // goes from 0 degrees to 180 degrees in steps of 1 degree
-    servo_one.write(head_unit_one_pos);                                             // tell servo to go to position in variable 'pos'
+  for (head_unit_one_pos = 180; head_unit_one_pos >= headUnitOnePosDown; head_unit_one_pos -= 5){     // goes from 180 degrees to 0 degrees
+    servo_one.write(head_unit_one_pos);                                                               // tell servo to go to position in variable 'pos'
   }
 }
 
@@ -116,7 +110,27 @@ void ServoMotorWrapped(){
     delayMicroseconds(delayMicro);
   }
   delay(1000); // Wait a second
-  
+}
+
+void NeedleRotation(){
+  for (head_unit_two_pos = 180; head_unit_two_pos >= headUnitTwoPosDown; head_unit_two_pos -= 5) {   // goes from 0 degrees to 180 degrees in steps of 1 degree
+    servo_two.write(head_unit_two_pos);                                                              // tell servo to go to position in variable 'pos'
+  }
+}
+
+void LiftingNeedleUp(){
+  for (head_unit_one_pos = 0; head_unit_one_pos <= headUnitOnePosUp; head_unit_one_pos += 5){    // goes from 0 degrees to 180 degrees in steps of 1 degree
+    servo_one.write(head_unit_one_pos);                                                          // tell servo to go to position in variable 'pos'
+  }
+}
+
+void NeedleRotationBack(){
+  for (head_unit_two_pos = 0; head_unit_two_pos <= headUnitTwoPosUp; head_unit_two_pos += 5) {   // goes from 0 degrees to 180 degrees in steps of 1 degree
+    servo_two.write(head_unit_two_pos);                                                          // tell servo to go to position in variable 'pos'
+  }
+}
+
+void ServoMotorWrappedBack(){
   digitalWrite(dirPin, HIGH);
    
   // Spin motor quickly
@@ -127,23 +141,4 @@ void ServoMotorWrapped(){
     delayMicroseconds(delayMicro);
   }
   delay(1000); // Wait a second
-}
-
-void NeedleRotation(){
-  for (head_unit_two_pos = 180; head_unit_two_pos >= 0; head_unit_two_pos -= 5){     // goes from 180 degrees to 0 degrees
-    servo_two.write(head_unit_two_pos);                                              // tell servo to go to position in variable 'pos'
-  }
-}
-
-void LiftingNeedleUp(){
-  for (head_unit_one_pos = 180; head_unit_one_pos >= 0; head_unit_one_pos -= 5){     // goes from 180 degrees to 0 degrees
-    servo_one.write(head_unit_one_pos);                                              // tell servo to go to position in variable 'pos'
-  }
-  
-}
-
-void NeedleRotationBack(){
-  for (head_unit_two_pos = 0; head_unit_two_pos <= 180; head_unit_two_pos += 5) {   // goes from 0 degrees to 180 degrees in steps of 1 degree
-  servo_two.write(head_unit_two_pos);                                                 // tell servo to go to position in variable 'pos'
-  }
 }
